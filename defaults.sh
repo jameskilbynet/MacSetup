45e8699f7ee38d0ef406323d79fce738c2004c8c
James Kilby <james@jameskilby.net>
2018-12-04 17:37:27 +0000

    Added Mac Settings

---
+++ defaults.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Adapted from https://github.com/pathikrit/mac-setup-script/raw/master/defaults.sh
+
+if [[ -z "${CI}" ]]; then
+  sudo -v # Ask for the administrator password upfront
+  # Keep-alive: update existing `sudo` time stamp until script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
+# Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+
+# Set Desktop as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+
+chflags nohidden ~/Library     # Show the ~/Library folder
+sudo chflags nohidden /Volumes # Show the /Volumes folder
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
