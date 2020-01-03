da093feed3f5f07c1d04608b89c6191251f6a2f3
James Kilby <james.kilby@gmail.com>
2020-01-03 15:02:08 +0000

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -29,10 +29,7 @@ mas install 485812721
 #todoist 
 mas install 585829637
 #pocket
-mas install 568494494b
-#lastpass
 mas install 926036361
 #ihosts
 mas install 1102004240 
-# Gemini 2
-mas install 1090488118 
+
